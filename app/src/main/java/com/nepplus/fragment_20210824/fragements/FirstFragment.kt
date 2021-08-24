f6faa5297ee0094fa65260dd6a6034392705b2d4
김현지 <guswlhjk@naver.com>
2021-08-24 22:06:55 +0900

    첫 프래그먼트를 xml에 올리기

--- app/src/main/java/com/nepplus/fragment_20210824/fragements/FirstFragment.kt
+++ app/src/main/java/com/nepplus/fragment_20210824/fragements/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.nepplus.fragment_20210824.fragements
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.nepplus.fragment_20210824.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -18,6 +21,25 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+
+    }
+
+//동작 관련 코드들이 작성되는 함수 =>액티비티 : onCreate
+//이 클래스 : 화면 X , 화면에 올라가는 부품 ()
+//프래그먼트의 mCreate는 화면도 준비되기 전 => 동작시키면 에러 소지가 높다
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    loginBtn.setOnClickListener {
+        Log.d("부품에서 찍기", "로그버튼 눌림")
+    }
+
+
+
+
+
+        }
+
 
     }
 
